65763030ece6287d23ddb89c77148ccfac73b88e
TheStek <sforte99@gmail.com>
2021-10-22 13:09:27 +0100

    Get postcodes + fix file path in prepare_file_v2

--- Code/prepare_file_v2.py
+++ Code/prepare_file_v2.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
-flowSpec = pd.read_csv("Data Spec Files\\Flow.csv")
-flowFile = pd.read_fwf("Data\\Split Out Data\\flow", widths = flowSpec["Length"], header = None, names = flowSpec["Field Name"])
+flowSpec = pd.read_csv(r"C:\Users\User\Documents\4. Fourth Year\Project\Railway-Fares\Data Spec Files\\Flow.csv")
+flowFile = pd.read_fwf(r"C:\Users\User\Documents\4. Fourth Year\Project\Railway-Fares\Data\Raw Fare Data\Split Out Data\flow", widths = flowSpec["Length"], header = None, names = flowSpec["Field Name"])
 print(flowFile.head())
+input()

cd9789e2291dfdb0477a40da4a66d6eb48a620af
TheStek <sforte99@gmail.com>
2022-01-06 16:04:29 +0000

    Added R script to read log files

---
+++ Code/3. ML/Results.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(jsonlite)
+
+
+log_path <- "C:/Users/User/Documents/4. Fourth Year/Project/Railway-Fares/Logs/"
+
+
+
+lin_reg <- fromJSON(paste(log_path, "linear_regression.json", sep = ""))
+
+lin_reg %>% arrange(desc(val_score))
+
+rfr <- fromJSON(paste(log_path, "random_forest_regression.json", sep = ""))
+
+rfr %>% arrange(desc(val_score))

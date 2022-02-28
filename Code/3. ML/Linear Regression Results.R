e59079dbff55bbd1453118228ccffee395175e72
TheStek <sforte99@gmail.com>
2022-02-28 21:15:16 +0000

    SVR Results

--- Code/3. ML/Linear Regression Results.R
+++ Code/3. ML/Linear Regression Results.R
@@ -16,4 +16,22 @@ lin_reg %>%
   mutate(se.val_score = sd.val_score / sqrt(n.val_score),
          ci_width = qt(1 - (0.025 / 2), n.val_score - 1) * se.val_score,
          lower.ci.val_score = mean.val_score - ci_width,
-         upper.ci.val_score = mean.val_score + ci_width)
+         upper.ci.val_score = mean.val_score + ci_width) %>%
+  mutate(width = formatC(ci_width, format = "e", digits = 2)) %>%
+  select(method, subset, mean.val_score, width) %>%
+  arrange(subset)
+
+
+subset_mapping <- tibble(
+  code_name = c('dist', 'dist_time', 'dist_remoteness', 'full', 'simd'),
+  name = c('Distance', 'Distance + Time', 'Distance + Remoteness', 'Full', 'SIMD'))
+
+ggplot(data = lin_reg %>%
+         filter(val_score > 0.7) %>%
+         inner_join(subset_mapping, by = c("subset" = "code_name")) %>%
+         mutate(desc = ifelse(grepl("pca", method), paste("PCA", name, sep = " - "), name))
+)+
+  geom_boxplot(aes(x = reorder(desc, val_score), y = val_score), color = "black", fill = "blue")+
+  coord_flip()+
+  xlab("Data Subset")+
+  ylab("Validation Score")
--- Code/3. ML/SVR Results.R
+++ Code/3. ML/SVR Results.R
@@ -6,9 +6,9 @@ log_path <- "C:/Users/User/Documents/4. Fourth Year/Project/Railway-Fares/Logs/"
 
 
 
-rfr <- fromJSON(paste(log_path, "random_forest_regression.json", sep = ""))
+svr <- fromJSON(paste(log_path, "svr.json", sep = ""))
 
-rfr %>%
+svr %>%
   group_by(method, dataset, subset) %>%
   summarise(mean.val_score = mean(val_score),
             sd.val_score = sd(val_score),
@@ -16,23 +16,22 @@ rfr %>%
   mutate(se.val_score = sd.val_score / sqrt(n.val_score),
          ci_width = qt(1 - (0.025 / 2), n.val_score - 1) * se.val_score,
          lower.ci.val_score = mean.val_score - ci_width,
-         upper.ci.val_score = mean.val_score + ci_width) %>%
+         upper.ci.val_score = mean.val_score + ci_width)%>%
   mutate(width = formatC(ci_width, format = "e", digits = 2)) %>%
-  select(method, subset, mean.val_score, width) %>%
-  filter(method == 'pca_rfr')
+  select(method, subset, mean.val_score, width)
 
 
 subset_mapping <- tibble(
   code_name = c('dist', 'dist_time', 'dist_remoteness', 'full', 'simd'),
   name = c('Distance', 'Distance + Time', 'Distance + Remoteness', 'Full', 'SIMD'))
 
-ggplot(data = rfr %>%
-         filter(val_score > 0.96) %>%
+ggplot(data = svr %>%
          inner_join(subset_mapping, by = c("subset" = "code_name")) %>%
-         mutate(desc = ifelse(grepl("pca", method), paste("PCA", name, sep = " - "), name))
+         filter(val_score > 0.9) %>%
+         mutate(desc = ifelse(grepl("lin", method), paste("Linear", name, sep = " - "), paste("RBF", name, sep = " - ")))
 )+
   geom_boxplot(aes(x = reorder(desc, val_score), y = val_score), color = "black", fill = "blue")+
   coord_flip()+
-  xlab("Data Subset")+
+  xlab("Kernel + Data Subset")+
   ylab("Validation Score")
 
